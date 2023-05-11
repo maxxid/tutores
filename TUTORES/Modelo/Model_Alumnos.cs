cef57d633102a2881cd16f4b47cd9ada5ad22931
maxxid <32134786+maxxid@users.noreply.github.com>
2023-05-11 14:56:12 -0300

    comiitnew

---
+++ TUTORES/Modelo/Model_Alumnos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TUTORES.Modelo
+{
+    public class Model_Alumnos
+    {
+        public string Nombre{ get; set; }
+        public string Apellido{ get; set; }
+        public string Icono { get; set; }
+        public string Estado_Asistencia { get; set; }
+        public string Fecha { get; set; }
+    }
+}
