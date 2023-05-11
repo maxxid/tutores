6b1d4c310be57f4ccb680c0db46590ca92b82893
maxxid <32134786+maxxid@users.noreply.github.com>
2023-05-11 14:45:37 -0300

    commit for push
    Signed-off-by: maxxid <32134786+maxxid@users.noreply.github.com>

--- TUTORES/VistaModelo/PageAsistenciasPorCursoVM.cs
+++ TUTORES/VistaModelo/PageAsistenciasPorCursoVM.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using TUTORES.Modelo;
 using Xamarin.Forms;
 
 namespace TUTORES.VistaModelo
@@ -8,14 +10,48 @@ namespace TUTORES.VistaModelo
     class PageAsistenciasPorCursoVM : BaseViewModel
     {
 
+        public ObservableCollection<Model_Alumnos> AlumnosList { get; set; }
 
 
         public PageAsistenciasPorCursoVM(INavigation navigation)
         {
             Navigation = navigation;
+            InitProducts();
         }
 
+        private void InitProducts()
+        {
+            AlumnosList = new ObservableCollection<Model_Alumnos>
+            {
+                new Model_Alumnos
+                {
+                    Nombre = "Anabel Perez",
+                    Apellido = "Perez",
+                    Icono = "monkey.png",
+                    Estado_Asistencia = "P",
+                    Fecha = ""
+                },
+                new Model_Alumnos
+                {
+                    Nombre = "Belen Maroni",
+                    Apellido = "Maroni",
+                    Icono = "monkey.png",
+                    Estado_Asistencia = "P",
+                    Fecha = ""
+                },
+                new Model_Alumnos
+                {
+                    Nombre = "Candela Pieroni",
+                    Apellido = "Pieroni",
+                    Icono = "monkey.png", Estado_Asistencia = "P", Fecha = ""
+                },
+                new Model_Alumnos
+                {Nombre = "Daniela Benakovic",
+                    Apellido = "Benakovic", 
+                    Icono = "monkey.png", Estado_Asistencia = "P", Fecha = ""
 
-
+                }
+            };
+        }
     }
 }
