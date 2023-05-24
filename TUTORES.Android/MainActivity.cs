036e740b800d60bcea235957c9a18c51f246b82e
Maximiliano Barconte <maxi@barconte.com.ar>
2023-05-24 18:16:38 -0300

    icono de aplicacion y escalas

--- TUTORES.Android/MainActivity.cs
+++ TUTORES.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using TUTORES.Vistas.Principal;
 
 namespace TUTORES.Droid
 {
-    [Activity(Label = "TUTORES", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "TUTORES", Icon = "@mipmap/iconnew", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
