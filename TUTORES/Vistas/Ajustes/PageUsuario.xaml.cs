1412717e7f265f07dccb247f01cd32a959c14df7
Maximiliano Barconte <maxi@barconte.com.ar>
2023-05-31 17:49:36 -0300

    Actualizaciones graficas en userpage: agregado de encabezado, contenedor dinamico, implento boton EDITAR, usericon, titles, entrys con futuro binding a datos, cambio de colores predeterminados para una correcta visualizacion, propiedades visuales dinamicas, botones. Biding view model, variables y funciones, agregado de diez iconos para implementacion .

--- TUTORES/Vistas/Ajustes/PageUsuario.xaml.cs
+++ TUTORES/Vistas/Ajustes/PageUsuario.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TUTORES.VistaModelo.Principal;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,8 @@ namespace TUTORES.Vistas.Ajustes
         public PageUsuario()
         {
             InitializeComponent();
+            BindingContext = new PageUsuarioVM(Navigation);
+
         }
     }
 }
